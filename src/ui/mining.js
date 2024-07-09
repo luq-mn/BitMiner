5efb2211b00887bd3b9d1510b4f39a570a626c14
luq-mne <luqman.yazrie@outlook.com>
2024-07-10 00:03:36 +0800

    topBar buttons fr

--- src/ui/mining.js
+++ src/ui/mining.js
@@ -1,6 +1,9 @@
+import { topBar } from './topBar';
+
 export function MiningScene() {
     k.scene("mining", () => {
         // Title text
         
+        bar = topBar()
     })
 }
--- src/ui/topBar.js
+++ src/ui/topBar.js
@@ -3,23 +3,69 @@ export function topBar() {
     k.onKeyPress("q" || "Q", () => {
         console.log("Changing to mining scene")
         k.go("mining")
+    });
+
+    miningFrame = k.add([
+        k.rect(190, 45),
+        k.pos(10, 10),
+        k.color(255, 255, 255),
+        k.area()
+    ])
+
+    miningText = k.add([
+        k.text("[Q] Mining"),
+        k.pos(20, 20),
+        k.color(20, 20, 20),
+        k.scale(0.75),
+        k.area()
+    ])
+
+    miningFrame.onHoverUpdate(() => {
+        miningFrame.color = k.rgb(200, 200, 200)
+    })
+
+    miningFrame.onHoverEnd(() => {
+        miningFrame.color = k.rgb(255, 255, 255)
     })
 
     // Wallet scene
     k.onKeyPress("w" || "W", () => {
         console.log("Changing to wallet scene")
         k.go("wallet")
+    });
+
+    walletFrame = k.add([
+        k.rect(190, 45),
+        k.pos(210, 10),
+        k.color(255, 255, 255),
+        k.area()
+    ])
+
+    walletText = k.add([
+        k.text("[W] Wallet"),
+        k.pos(220, 20),
+        k.color(20, 20, 20),
+        k.scale(0.75),
+        k.area()
+    ])
+
+    walletFrame.onHoverUpdate(() => {
+        walletFrame.color = k.rgb(200, 200, 200)
+    })
+
+    walletFrame.onHoverEnd(() => {
+        walletFrame.color = k.rgb(255, 255, 255)
     })
 
     // Exchange scene
     k.onKeyPress("e" || "E", () => {
         console.log("Changing to exchange scene")
         k.go("exchange")
-    })
+    });
 
     // Store scene
     k.onKeyPress("s" || "S", () => {
         console.log("Changing to store scene")
         k.go("store")
-    })
+    });
 }
